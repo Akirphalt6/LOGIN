86a1547e6df7b737597745bf49581a0d262f7653
Akirphalt <77574895+Akirphalt6@users.noreply.github.com>
2022-03-23 19:45:25 +0900

    LoginView の追加

---
+++ View/LoginView.java
@@ -0,0 +1,13 @@
+package View;
+import Controller.LoginController;
+
+public class LoginView{
+    
+    public void enter(){
+        
+        LoginController logincontroller = new LoginController();
+        logincontroller.login();
+
+    }
+
+}
